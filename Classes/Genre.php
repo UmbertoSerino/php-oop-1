1df20c9834a495ee3ed85b38e46b4f9d21dc2774
UmbertoSerino <serumberto@gmail.com>
2024-01-26 00:44:58 +0100

    inserted php doc and typing discipline value in construction function

--- Classes/Genre.php
+++ Classes/Genre.php
@@ -1,9 +1,20 @@
-<?php 
-class Genre {
-    public $type
+<?php
+class Genre
+{
+    public $type;
 
-    public function __construct($_type)
+    /**
+     * constructor of the genre type
+     *
+     * @param string genre of the film
+     */
+    public function __construct(string $_type)
     {
-        $this->$type = $_type;
+        $this->type = $_type;
     }
-} 
+}
+
+$commedyGenre = new Genre('Commedy');
+$actionGenre = new Genre('Action');
+$thrillerGenre = new Genre('Thriller');
+$westernGenre = new Genre('Western');
